65a8245d74a889d7ff86092834b59596768d21f3
Datka <datkascnu@gmail.com>
2022-10-19 22:06:25 +0600

    workspace's methods

--- src/main/java/kg/peaksoft/taskTrackerb6/api/WorkspaceApi.java
+++ src/main/java/kg/peaksoft/taskTrackerb6/api/WorkspaceApi.java
@@ -12,6 +12,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.mail.MessagingException;
 import java.util.List;
 
