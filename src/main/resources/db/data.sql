9c257fce98e0ff7200a6b17dae4ac626d6e5e969
AkmatovT <tynchtyk7@mail.ru>
2022-11-07 14:13:36 +0600

    fixed

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -20,7 +20,7 @@ values (1, 'First Column', false, 1);
 insert into cards (id, title, description, is_archive, created_at, creator_id, board_id, column_id)
 values (1, 'First Card', 'First Description', false, '2022-10-31',  1, 1, 1);
 
-insert into checklists (id, title, task_tracker, card_id)
+insert into checklists (id, title, count, card_id)
 values (1, 'First Checklist', 1, 1);
 
 insert into times (id, hour, minute)
