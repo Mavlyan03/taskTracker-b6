2f0ff76fcf1970ad8fb53efedf4a679f4255fa80
Mamatisaev <umamatisaev@gmail.com>
2022-11-08 12:04:41 +0600

    Label Service Label Response Label Request Label Update Request Label Repository

--- src/main/java/kg/peaksoft/taskTrackerb6/api/LabelController.java
+++ src/main/java/kg/peaksoft/taskTrackerb6/api/LabelController.java
@@ -2,12 +2,15 @@ package kg.peaksoft.taskTrackerb6.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kg.peaksoft.taskTrackerb6.db.service.LabelService;
+import kg.peaksoft.taskTrackerb6.dto.request.LabelUpdateRequest;
+import kg.peaksoft.taskTrackerb6.dto.response.LabelResponse;
+import kg.peaksoft.taskTrackerb6.dto.response.SimpleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import kg.peaksoft.taskTrackerb6.db.service.LabelService;
-import kg.peaksoft.taskTrackerb6.dto.request.LabelUpdateRequest;
-import kg.peaksoft.taskTrackerb6.dto.response.SimpleResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/labels")
@@ -19,12 +22,24 @@ public class LabelController {
 
     private final LabelService labelService;
 
-    @Operation(summary = "Description update method",
-            description = "Update description by label id")
+    @Operation(summary = "Update abel",
+            description = "Update label by id")
     @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{id}")
     public SimpleResponse updateDescription(@PathVariable Long id,
                                             @RequestBody LabelUpdateRequest labelUpdateRequest) {
         return labelService.updateLabel(id, labelUpdateRequest);
+    }
+
+    @Operation(summary = "Get label", description = "Get label by id")
+    @GetMapping("{id}")
+    public LabelResponse getById(@PathVariable Long id) {
+        return labelService.getLabelById(id);
+    }
+
+    @Operation(summary = "Get all labels", description = "Get all labels by card id")
+    @GetMapping("list/{id}")
+    public List<LabelResponse> getAllLabelsByCardId(@PathVariable Long cardId) {
+        return labelService.getAllLabelsByCardId(cardId);
     }
 }
--- src/main/java/kg/peaksoft/taskTrackerb6/db/service/LabelService.java
+++ src/main/java/kg/peaksoft/taskTrackerb6/db/service/LabelService.java
@@ -1,13 +1,16 @@
 package kg.peaksoft.taskTrackerb6.db.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import kg.peaksoft.taskTrackerb6.db.model.Label;
 import kg.peaksoft.taskTrackerb6.db.repository.LabelRepository;
 import kg.peaksoft.taskTrackerb6.dto.request.LabelUpdateRequest;
+import kg.peaksoft.taskTrackerb6.dto.response.LabelResponse;
 import kg.peaksoft.taskTrackerb6.dto.response.SimpleResponse;
 import kg.peaksoft.taskTrackerb6.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -27,4 +30,19 @@ public class LabelService {
         return new SimpleResponse("Updated", "OK");
     }
 
+    public LabelResponse getLabelById(Long id) {
+        Label label = labelRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Label with id: " + id + " not found!")
+        );
+
+        return new LabelResponse(
+                label.getId(),
+                label.getDescription(),
+                label.getColor()
+        );
+    }
+
+    public List<LabelResponse> getAllLabelsByCardId(Long cardId) {
+        return labelRepository.getAllLabelResponses(cardId);
+    }
 }
