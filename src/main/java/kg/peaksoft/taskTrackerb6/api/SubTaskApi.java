67f41eb8dbde7e0fab6862f33a8c961099ace499
AkmatovT <tynchtyk7@mail.ru>
2022-11-17 17:10:00 +0600

    SubTask

--- src/main/java/kg/peaksoft/taskTrackerb6/api/SubTaskApi.java
+++ src/main/java/kg/peaksoft/taskTrackerb6/api/SubTaskApi.java
@@ -1,10 +1,12 @@
 package kg.peaksoft.taskTrackerb6.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kg.peaksoft.taskTrackerb6.db.service.SubTaskService;
+import kg.peaksoft.taskTrackerb6.dto.request.SubTaskRequest;
+import kg.peaksoft.taskTrackerb6.dto.response.SubTaskResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Subtask API", description = "All endpoints of subtask")
 public class SubTaskApi {
 
+    private final SubTaskService subTaskService;
 
+    @Operation(summary = "Create subtask", description = "Create new subtask")
+    @PostMapping("{id}")
+    public SubTaskResponse createSubTask(@PathVariable Long id,
+                                         @RequestBody SubTaskRequest request){
+        return subTaskService.createSubTask(id, request);
+    }
 }
