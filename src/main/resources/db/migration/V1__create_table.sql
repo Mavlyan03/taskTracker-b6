c736d101245808575dbbbf1a426029c008a984e7
Datka <datkascnu@gmail.com>
2022-11-18 12:25:39 +0600

    ..

---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,8 @@
+create table users(
+    id integer primary key,
+    first_name varchar(255),
+    last_name varchar(255),
+    photo_link varchar(255),
+    email varchar(255),
+    password varchar(255)
+);
