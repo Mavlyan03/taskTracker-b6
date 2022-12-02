844ce7a3c3bfa02535f07090f1c4366cecd33a41
Datka <datkascnu@gmail.com>
2022-12-02 19:00:55 +0600

    fixed

--- src/main/java/kg/peaksoft/taskTrackerb6/db/service/MemberService.java
+++ src/main/java/kg/peaksoft/taskTrackerb6/db/service/MemberService.java
@@ -27,26 +27,36 @@ public class MemberService {
 
     public List<MemberResponse> searchByEmailOrName(Long id, String emailOrName) {
         Workspace workspace = workspaceRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Workspace with this ID is not found"));
-        return userRepository.searchByEmailOrName(emailOrName,workspace.getId());
+                () -> new NotFoundException("Workspace with id: " + id + " not found!")
+        );
+
+        return userRepository.searchByEmailOrName(emailOrName, workspace.getId());
     }
 
     public AllMemberResponse getAllMembers(Long id) {
         Card card = cardRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Card with this ID is not found"));
-        Board board = boardRepository.findById(card.getBoard().getId()).orElseThrow(
-                () -> new NotFoundException("Board with this ID is not found"));
-        Workspace workspace = workspaceRepository.findById(card.getWorkspace().getId()).orElseThrow(
-                () -> new NotFoundException("Workspace With this ID is not found"));
+                () -> new NotFoundException("Card with id: " + id + " not found!")
+        );
+
+        Board board = boardRepository.findById(card.getColumn().getBoard().getId()).orElseThrow(
+                () -> new NotFoundException("Board with id: " + card.getColumn().getBoard().getId() + " not found!")
+        );
+
+        Workspace workspace = workspaceRepository.findById(card.getColumn().getBoard().getWorkspace().getId()).orElseThrow(
+                () -> new NotFoundException("Workspace with id: " + board.getWorkspace().getId() + " not found!")
+        );
+
         AllMemberResponse members = new AllMemberResponse();
         List<MemberResponse> boardMembers = new ArrayList<>();
         List<MemberResponse> workspaceMembers = new ArrayList<>();
-        for(User user : board.getMembers()) {
+        for (User user : board.getMembers()) {
             boardMembers.add(new MemberResponse(user));
         }
-        for(UserWorkSpace workSpace : workspace.getUserWorkSpaces()) {
+        for (UserWorkSpace workSpace : workspace.getUserWorkSpaces()) {
             User user = userRepository.findUserByWorkSpaceId(workSpace.getId()).orElseThrow(
-                    () -> new NotFoundException("User with this ID is not found"));
+                    () -> new NotFoundException("User with id: " + workSpace.getId() + " not found!")
+            );
+
             workspaceMembers.add(new MemberResponse(user));
         }
         members.setBoardMembers(boardMembers);
@@ -57,9 +67,13 @@ public class MemberService {
 
     public MemberResponse assignMemberToCard(Long memberId, Long cardId) {
         User user = userRepository.findById(memberId).orElseThrow(
-                () -> new NotFoundException("User with this ID is not found"));
+                () -> new NotFoundException("User with id: " + memberId + " not found!")
+        );
+
         Card card = cardRepository.findById(cardId).orElseThrow(
-                () -> new NotFoundException("Card with this ID is not found"));
+                () -> new NotFoundException("Card with id: " + cardId + " not found!")
+        );
+
         card.addMember(user);
         cardRepository.save(card);
         return new MemberResponse(user);
